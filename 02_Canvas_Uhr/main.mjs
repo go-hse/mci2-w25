38bddcb3e8707de829043a081998973e2a3af366
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-10-07 12:22:15 +0200

    Uhr mit Sekundenzeiger

--- 02_Canvas_Uhr/main.mjs
+++ 02_Canvas_Uhr/main.mjs
@@ -1,5 +1,8 @@
 import { circle, line } from "./js/grafics.mjs"
 
+const fontSize = 24;
+
+
 window.onload = () => {
     let cnv = document.getElementById("cnv");
     let ctx = cnv.getContext("2d");
@@ -11,17 +14,25 @@ window.onload = () => {
     }
     addEventListener("resize", resize);
 
-    const inner = cnv.height * 0.4;
-    const outer = cnv.height * 0.42;
-
     const secondAngle = Math.PI / 30;
 
     function draw() {
+        const inner = cnv.height * 0.4;
+        const outer = cnv.height * 0.42;
+
         ctx.resetTransform();
         ctx.clearRect(0, 0, cnv.width, cnv.height);
 
         ctx.translate(cnv.width / 2, cnv.height / 2);
-        circle(ctx, 0, 0, 10, "red");
+
+        const now = new Date();
+        ctx.font = `${fontSize}px monospace`;
+        ctx.fillStyle = "#000";
+        const text = now.toLocaleTimeString();
+        const bb = ctx.measureText(text);
+        const bbheight = bb.actualBoundingBoxAscent + bb.actualBoundingBoxDescent;
+        ctx.fillText(text, -bb.width / 2, bbheight / 2);
+
 
         for (let i = 0; i < 60; i++) {
             if (i % 5 === 0) {
@@ -31,6 +42,15 @@ window.onload = () => {
             }
             ctx.rotate(secondAngle);
         }
+
+        ctx.resetTransform();
+        ctx.translate(cnv.width / 2, cnv.height / 2);
+
+        const actSeconds = now.getSeconds();
+        const actSecondsAngle = actSeconds * secondAngle;
+
+        ctx.rotate(actSecondsAngle);
+        line(ctx, 0, 0, 0, -outer, "#f00", 1);
 
 
         window.requestAnimationFrame(draw);
